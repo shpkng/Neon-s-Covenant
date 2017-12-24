19b096a4f6f25a1059b6f1a5cbced700b92faba6
LAPTOP-7V28TAK7\qyjbe <1798174328@163.com>
2017-12-24 16:54:28 +0800

    初始版本测试，相机、光路调试。

---
+++ Assets/CameraLook.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLook : MonoBehaviour {
+
+
+    public Transform player1;
+
+
+    [SerializeField]
+
+    Vector3 playerToLook;
+
+
+
+    private void Awake()
+    {
+        playerToLook = this.transform.position - player1.transform.position;
+    }
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        this.transform.position = player1.transform.position + playerToLook;
+		
+	}
+}
