ddc3ad883db8b9c1474f86533022fc585869f08b
shpkng <shpkng@gmail.com>
2018-01-15 23:41:20 +0800

    ThirdPersonUserControl.cs 从StandartAsset中复制并进行少量定制 ThirdPersonCharacter.cs 从StandartAsset中复制 Interact.cs 新增，用以挂载于场景中的可交互物体上，目前定义的可交互物体仅有Portal一种 更新了百度脑图 http://naotu.baidu.com/file/52392729566363b5ce5c7903d28e1db5

---
+++ Assets/_Script/Interact.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interact : MonoBehaviour
+{
+
+	public GameController gameController;
+	public string typeOfThisObject;
+
+
+	/// <summary>
+	/// 按E可赛艇
+	/// </summary>
+	public void E()
+	{
+		switch (typeOfThisObject)
+		{
+			case "Portal": Portal(); break;
+			case "Else":Else() ; break;
+		}
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		gameController.InteractableObject = this;
+	}
+
+	void Portal()
+	{;
+		Debug.Log("Portal");
+	}
+
+	void Else()
+	{
+		Debug.Log("Else");
+	}
+
+}
